7f275d803b2ef30c2c7106c9651336b5f73515cc
Ethan Nien <zifuera@outlook.com>
2026-07-06 10:53:30 +0800

    feat: load nav logo styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './index.css';
+import './navLogos.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />
